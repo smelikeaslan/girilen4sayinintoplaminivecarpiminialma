c9df19dfff96e95a0081aef976a379b17ad4355e
smelikeaslan <aslansumeyramelike@gmail.com>
2024-10-20 23:31:51 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Birinci sayıyı giriniz:");
+int birincisayi = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("İkinci sayıyı giriniz:");
+int ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Üçüncü sayıyı giriniz:");
+int ucuncusayi = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Dördüncü sayıyı giriniz:");
+int dorduncusayi = Convert.ToInt32(Console.ReadLine());
+
+int toplam = birincisayi + ikincisayi + ucuncusayi + dorduncusayi;
+Console.WriteLine("Dört sayının toplamı=" + toplam);
+
+int carpim = birincisayi * ikincisayi * ucuncusayi * dorduncusayi;
+Console.WriteLine("Dört sayının çarpımı=" + carpim);
+
+Console.ReadKey();
